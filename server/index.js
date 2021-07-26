fa86564e3f9888dd8369b2000b5a71c0a8cabaf1
Masoud <masoud_mdar@yahoo.fr>
2021-07-26 10:12:54 +0200

    server first configs

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Hello!")
+})
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`)
+})

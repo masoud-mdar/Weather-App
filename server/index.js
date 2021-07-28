e1f2f44037bc31bdbd3d79b6f1e84c5473f72089
Masoud <masoud_mdar@yahoo.fr>
2021-07-28 09:17:46 +0200

    heroku configs

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 
 const apiRoutes = require("./routes/api")
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
 
 apiRoutes(app)
 
-app.listen(PORT || 5000, () => {
+app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })

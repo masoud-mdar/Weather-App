7e51f76b6f36e3e3e4cbaa3036700c60e176c76c
Masoud <masoud_mdar@yahoo.fr>
2021-07-28 11:32:27 +0200

    bug fix

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
 const express = require("express")
 const cors = require("cors")
+const apiRoutes = require("./routes/api")
 require("dotenv").config()
 
-const apiRoutes = require("./routes/api")
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:"*"}))
 
-/*app.get("/", (req, res) => {
-    res.send("Hello!")
-})*/
+app.get("/", (req, res) => {
+    res.send("Hello !")
+})
 
 apiRoutes(app)
+
+app.use((req, res, next) => {
+    res.status(404)
+    .type("text")
+    .send("Page Not Found...")
+})
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)

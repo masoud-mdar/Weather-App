c81c919adc49befd6a6a0d91a7fd3e68b6b1a63c
Masoud <masoud_mdar@yahoo.fr>
2021-07-28 11:23:51 +0200

    bug fix

--- server/index.js
+++ server/index.js
@@ -4,19 +4,19 @@ require("dotenv").config()
 
 const apiRoutes = require("./routes/api")
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
     res.send("Hello!")
-})
+})*/
 
 apiRoutes(app)
 
-app.listen(PORT || 5000, () => {
+app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })
